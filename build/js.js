959cd7863176a25443bf76150716f6d8c8aa1643
William Wong <wwong@CZC2381K6T.mcr.local>
2015-11-06 11:56:46 +0000

    la

--- build/js.js
+++ build/js.js
@@ -10,6 +10,7 @@ var mapModule = (function() {
 
   var init = function() {
     initMap();
+    populateMarkers();
     getLocation();
     eventHandlers();
   }
@@ -67,21 +68,29 @@ var mapModule = (function() {
         map.panTo(new L.LatLng(currentLat, currentLng), {animate: true, duration: 4, zoom: 13});
         $('.logo img').css({'-webkit-filter': 'grayscale(0)'});
         // setTimeout(function(){ populateMarkers(); }, 2000);
-        populateMarkers();
       });
     });
 
     $('.leaflet-clickable').on('click', function() {
-
+      $('.gallery-panel').fadeIn(800);
+      getImages();
     });
+  }
+
+  var getImages() {
+    $.ajax({
+      url: 'http://teamsilva.azurewebsites.net/Images/GetImageJsonForLatLng?lat=51.4886&lng=-0.1207',
+    }).done(function(data) {
+
+    }
   }
 
   var populateMarkers = function() {
     $.ajax({
       url: 'http://teamsilva.azurewebsites.net/Images/GetEventDataJson',
     }).done(function(data) {
-      console.log(data);
-      var imageList = ImageList.ImageList;
+
+      var imageList = data;
 
       for (i=0; i < imageList.length; i++) {
         var marker = L.marker([imageList[i].Latitude, imageList[i].Longitude])
@@ -98,39 +107,3 @@ var mapModule = (function() {
 })();
 
 mapModule.init();
-
-var ImageList = {
-  "ImageList":[
-    {
-      "ImageUrl":"https://scontent.cdninstagram.com/hphotos-xpf1/t51.2885-15/s320x320/e35/11875507_1653712601567733_556205717_n.jpg",
-      "Latitude":51.4874013,
-      "Longitude":-0.124641122,
-      "Name":"89 Albert Embankment, Vauxhall"
-    },
-    {
-      "ImageUrl":"https://scontent.cdninstagram.com/hphotos-xpf1/t51.2885-15/s320x320/e35/11849882_842178569233812_2050902676_n.jpg",
-      "Latitude":51.4874013,
-      "Longitude":-0.124641122,
-      "Name":"89 Albert Embankment, Vauxhall"
-    },
-    {
-      "ImageUrl":"https://scontent.cdninstagram.com/hphotos-xft1/t51.2885-15/s320x320/e35/12063263_1508716202756008_1990820026_n.jpg",
-      "Latitude":51.4903,
-      "Longitude":-0.1193,
-      "Name":"Vauxhall"
-    },
-    {
-      "ImageUrl":"https://scontent.cdninstagram.com/hphotos-xap1/t51.2885-15/s320x320/e35/10848481_509297382579373_468583301_n.jpg",
-      "Latitude":51.4874013,
-      "Longitude":-0.124641122,
-      "Name":"89 Albert Embankment, Vauxhall"
-    },
-    {
-      "ImageUrl":"https://scontent.cdninstagram.com/hphotos-xfp1/t51.2885-15/s320x320/e35/11917880_137934793230197_1594623928_n.jpg",
-      "Latitude":51.4903,
-      "Longitude":-0.1193,
-      "Name":"Vauxhall"
-    }
-  ],
-  "Message":null
-}
